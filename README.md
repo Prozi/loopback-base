c64c3363a8ea7fdc6253bab6f5012e5ef48c58f6
Jacek Piętal <prozi85@gmail.com>
2016-05-21 23:24:07 +0200

    fetch data from rxnav api

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 # loopback-base
 Loopback app with mongodb, firebase, rest explorer
 
-# Usage - Step 1
+# Install
+
+## 1. Install dependencies
+
 ```bash
 git clone https://github.com/Prozi/loopback-base
 cd loopback-base
 npm install
-npm start
 ```
 
-# Usage - Step 2
+## 2. Add firebase creditentials
+
 You should put your firebase creditentials from json:
 > https://firebase.google.com/docs/auth/web/custom-auth#before-you-begin
 
 inside project root: `firebase.creditentials.json`
+
+## 3. Create database
+
+```bash
+npm run migrate
+```
+
+# Usage
+
+```bash
+npm start
+```
+
--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,30 +1,51 @@
 'use strict';
 
-var app = require('../server/server');
-var ds  = app.datasources.mongoDs;
+require('paint-console');
+
+var request = require('request');
+var rxnorm  = require('rxnorm-js');
+var app     = require('../server/server');
+var ds      = app.datasources.mongoDs;
 
 ds.automigrate('Medication', function(err) {
   if (err) throw err;
 
-  var medications = [
-    {
-      name: 'Paracetamol',
-      producer: 'Jacek',
-      price: 10
-    }
-  ];
+  console.info('importing drug list');
 
-  var count = medications.length;
-  medications.forEach(function(medication) {
-    app.models.Medication.create(medication, function(err, model) {
-      if (err) throw err;
+  request.get('https://rxnav.nlm.nih.gov/REST/allconcepts.json?tty=BN', function(err, res, json) {
+    if (err) throw err;
 
-      console.log('Created:', JSON.stringify(model, null, 2));
+    json = JSON.parse(json);
 
-      if (!--count) {
-        ds.disconnect();
-        process.exit();
+    var medications = [];
+    var group, concept;
+
+    for (group in json.minConceptGroup) {
+      if (json.minConceptGroup.hasOwnProperty(group)) {
+        for (concept in json.minConceptGroup[group]) {
+          if (json.minConceptGroup[group].hasOwnProperty(concept)) {
+            medications.push({
+              name: json.minConceptGroup[group][concept].name,
+              price: 10,
+              producer: 'Jacek'
+            });
+          }
+        }
       }
+    }
+
+    var count = medications.length;
+    medications.forEach(function(medication) {
+      app.models.Medication.create(medication, function(err, model) {
+        if (err) throw err;
+
+        if (!--count) {
+          ds.disconnect();
+          console.info('importing drug list -> success');
+          process.exit();
+        }
+      });
     });
   });
+
 });
