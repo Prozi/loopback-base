13ee5edbffcd1946c8b43a9f79ee4b80e466a5ab
Jacek Piętal <prozi85@gmail.com>
2016-05-22 01:04:47 +0200

    angular material for search box

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,8 +1,45 @@
 'use strict';
 
-(function () {
+(function() {
 
-  var ngApp = angular.module('ngApp', ['ngRoute']);
+  var ngApp = angular.module('ngApp', ['ngMaterial', 'ngRoute']);
+
+  ngApp.controller('SearchCtrl', ['$http', '$q', '$log', function($http, $q, $log) {
+
+    var self = this;
+
+    self.name        = 'SearchCtrl';
+    self.isDisabled  = false;
+    self.states      = [];
+    self.querySearch = querySearch;
+    self.selectedItemChange = selectedItemChange;
+
+    function getApi(query) {
+      var deferred = $q.defer();
+      $http({
+        url: query, 
+        method: 'GET'
+      })
+      .success(function(json) {
+        deferred.resolve(json.map(function(medication) {
+          return {
+            value: medication.name.toLowerCase(),
+            display: medication.name
+          };
+        })); 
+      });
+      return deferred.promise;
+    }
+
+    function querySearch (query) {
+      return getApi('/api/Medications?filter[where][name][regexp]=' + `/${query}/i`);
+    }
+
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    }
+
+  }]);
 
   ngApp.config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
@@ -13,8 +50,32 @@
           controllerAs: 'home'
         })
         .when('/search', {
-          template: 'search',
-          controller: 'DummyCtrl',
+          template: `
+            <md-content class="md-padding">
+              <form ng-submit="$event.preventDefault()">
+                <p>Search for medication.</p>
+                <md-autocomplete
+                    ng-disabled="search.isDisabled"
+                    md-no-cache="search.noCache"
+                    md-selected-item="search.selectedItem"
+                    md-search-text-change="search.searchTextChange(search.searchText)"
+                    md-search-text="search.searchText"
+                    md-selected-item-change="search.selectedItemChange(item)"
+                    md-items="item in search.querySearch(search.searchText)"
+                    md-item-text="item.display"
+                    md-min-length="0"
+                    placeholder="Please write name of medication">
+                  <md-item-template>
+                    <span md-highlight-text="search.searchText" md-highlight-flags="^i">{{item.display}}</span>
+                  </md-item-template>
+                  <md-not-found>
+                    No medications matching "{{search.searchText}}" were found.
+                  </md-not-found>
+                </md-autocomplete>
+              </form>
+            </md-content>
+          `,
+          controller: 'SearchCtrl',
           controllerAs: 'search'
         })
         .otherwise({
@@ -57,7 +118,7 @@
     };
   });
 
-  ngApp.directive('appbody', function () {
+  ngApp.directive('appbody', function() {
     return {
       template: `
         <div id="body" class="container">
