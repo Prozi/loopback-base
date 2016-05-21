1ea327405e05d4eb2be757d57c700fca841f7da7
Jacek Piętal <prozi85@gmail.com>
2016-05-22 00:25:19 +0200

    basic angular1 router

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,52 +1,85 @@
 'use strict';
 
-var ngApp = angular.module('ngApp', []);
+(function () {
 
-ngApp.directive('navbar', function() {
-  return {
-    template: `
-      <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand">
-              <img alt="LoopBack Base" src="favicon.ico">
-            </a>
+  var ngApp = angular.module('ngApp', ['ngRoute']);
+
+  ngApp.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/home', {
+          template: 'home',
+          controller: 'DummyCtrl',
+          controllerAs: 'home'
+        })
+        .when('/search', {
+          template: 'search',
+          controller: 'DummyCtrl',
+          controllerAs: 'search'
+        })
+        .otherwise({
+          redirectTo: '/home'
+        });
+
+      $locationProvider.html5Mode(true);
+  }]);
+
+  ngApp.controller('DummyCtrl', ['$routeParams', function($routeParams) {
+    this.name = 'DummyCtrl';
+    this.params = $routeParams;
+  }]);
+
+  ngApp.directive('navbar', function() {
+    return {
+      template: `
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand">
+                <img alt="LoopBack Base" src="favicon.ico">
+              </a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li><a href="/home">Home</a></li>
+                <li><a href="/search">Search</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
           </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li><a ng-route="home">Home</a></li>
-              <li><a ng-route="search">Search</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
+        </nav>
+      `    
+    };
+  });
+
+  ngApp.directive('appbody', function () {
+    return {
+      template: `
+        <div id="body" class="container">
+          <div ng-view>
+            <p>App started :)</p>
+          </div>
         </div>
-      </nav>
-    `    
-  };
-});
+      `
+    }
+  });
 
-ngApp.directive('appbody', function () {
-  return {
-  	template: `
-  	  <p id="body" class="container">App started :)</p>
-  	`
-  }
-});
+  ngApp.directive('app', function() {
+    return {
+      template: `
+        <navbar></navbar>
+        <appbody></appbody>
+      `
+    };
+  });
 
-ngApp.directive('app', function() {
-  return {
-    template: `
-      <navbar></navbar>
-      <appbody></appbody>
-    `
-  };
-});
+  angular.element(document).ready(function() {
+    angular.bootstrap(document, ['ngApp']);
+  });
 
-angular.element(document).ready(function() {
-  angular.bootstrap(document, ['ngApp']);
-});
+})();
