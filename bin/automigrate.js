e3e43b31e55790c5a1355905355294001fe24d32
Jacek Piętal <prozi85@gmail.com>
2016-05-26 23:22:39 +0200

    migration upgrade: get all term types

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -2,24 +2,53 @@
 
 require('paint-console');
 
-var request = require('request');
-var app     = require('../server/server');
-var ds      = app.datasources.mongoDs;
-var api     = 'https://rxnav.nlm.nih.gov/REST/allconcepts.json?tty=BN';
+let request = require('request');
+let app     = require('../server/server');
+let ds      = app.datasources.mongoDs;
+let api     = 'https://rxnav.nlm.nih.gov/REST/';
 
-ds.automigrate('Medication', function(err) {
-  if (err) throw err;
+let medications = [];
+let createMedications = () => {
+  // unique
+  medications = [...new Set(medications)];
+  let count = medications.length;
+  let start = count + 0;
+  console.info(`creating ${count} medication models in mongo`);
+  console.info(`this will take a while (<5min)`);
+  console.info(`we will tell you when we're over`);
+  medications.forEach((medication) => {
+    app.models.Medication.create(medication, (err, model) => {
+      if (err) throw err;
+      if (!--count) {
+        ds.disconnect();
+        console.info('importing drug list -> success');
+        process.exit();
+      } else {
+        process.stdout.write(`\rprogress: ${start - count}/${start}`);
+      }
+    });
+  });  
+};
 
-  console.info('importing drug list');
-
-  request.get(api, function(err, res, json) {
+let termTypesCount = 0; 
+let getTermTypes = () => {
+  request.get(`${api}termtypes.json`, (err, res, json) => {
     if (err) throw err;
-
+    let termType;
     json = JSON.parse(json);
+    json.termTypeList.termType.forEach((item) => {
+      termTypesCount++;
+      getConcepts(item);
+    });
+  });  
+};
 
-    var medications = [];
-    var group, concept, medication;
-
+let getConcepts = (termType) => {
+  console.info(`getting concepts for term type: ${termType}`);
+  request.get(`${api}allconcepts.json?tty=${termType}`, (err, res, json) => {
+    if (err) throw err;
+    let group, concept, medication;
+    json = JSON.parse(json);
     for (group in json.minConceptGroup) {
       if (json.minConceptGroup.hasOwnProperty(group)) {
         for (concept in json.minConceptGroup[group]) {
@@ -33,19 +62,14 @@ ds.automigrate('Medication', function(err) {
         }
       }
     }
+    if (!--termTypesCount) {
+      createMedications();
+    }
+  });  
+};
 
-    var count = medications.length;
-    medications.forEach(function(medication) {
-      app.models.Medication.create(medication, function(err, model) {
-        if (err) throw err;
-
-        if (!--count) {
-          ds.disconnect();
-          console.info('importing drug list -> success');
-          process.exit();
-        }
-      });
-    });
-  });
-
+ds.automigrate('Medication', (err) => {
+  if (err) throw err;
+  console.info('importing drug list');
+  getTermTypes();
 });
